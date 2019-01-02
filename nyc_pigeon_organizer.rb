331efab580d5cc02da3e40270bc3e7c752b5e750
Stephanie Messad <sabud1208@gmail.com>
2019-01-02 02:31:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         z[name] ||= {}
         z[name][x]= []
-        z[name][x] << colors
+        z[name][x] << colors.uniq
       end
     end
   end
