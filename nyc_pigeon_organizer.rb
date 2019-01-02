3a796c5b8cbe6fea4f5537eefedfd9dc6e270de3
Stephanie Messad <sabud1208@gmail.com>
2019-01-02 02:22:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(x, y), z|
     y.each do |color, names|
       names.each do |name|
+        z[name] ||= {}
+        z[name][Key]= []
+        z[name][key] << color.to_s
       end
     end
   end
