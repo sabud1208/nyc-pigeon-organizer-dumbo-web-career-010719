f4df2b76b8d35dddb50f7c5e6d0ec2200e902ccb
Stephanie Messad <sabud1208@gmail.com>
2019-01-02 02:13:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   data.each do |x, y|
-    y.each do |color|
-      color.each do |name|
+    y.each do |colors|
+      colors.each do |colors, name|
           binding.pry
         end
     end
