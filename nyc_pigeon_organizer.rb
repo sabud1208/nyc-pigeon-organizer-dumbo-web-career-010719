5870646e4ae460d60b18b4b66065099f1e533c56
Stephanie Messad <sabud1208@gmail.com>
2019-01-02 02:24:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,12 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(x, y), z|
-    y.each do |color, names|
+    y.each do |colors, names|
       names.each do |name|
         binding.pry
         z[name] ||= {}
         z[name][Key]= []
-        z[name][key] << color.to_s
+        z[name][key] << colors.to_s
       end
     end
   end
